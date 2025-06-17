e838cdb18bfd7e1fb00a8d1b9c4fd7e994f327ef
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-06-17 16:02:44 +0800

    Time: 1304 ms (7.32%), Space: 20.9 MB (56.1%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def numberOfAlternatingGroups(self, colors, k):
+        """
+        :type colors: List[int]
+        :type k: int
+        :rtype: int
+        """
+        m = len(colors)
+        colors.extend(colors)
+        print(colors)
+
+        prev = -1
+        n = len(colors)
+      
+
+        prev = -1
+        left = count = 0
+        for i in range(m+k-1):
+            if prev == colors[i]:
+                left = i
+            
+            while  i - left + 1 > k:
+                
+                left += 1
+                
+            if i - left + 1 == k:
+                count += 1
+            prev = colors[i]
+        return count
