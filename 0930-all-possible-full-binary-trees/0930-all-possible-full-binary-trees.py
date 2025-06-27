8d2043acb86f8d6726729a6ab6f75817903aa6d2
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-06-27 14:22:07 +0900

    Time: 91 ms (12.77%), Space: 41.6 MB (11.7%) - LeetHub

---
+++ 0930-all-possible-full-binary-trees/0930-all-possible-full-binary-trees.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def allPossibleFBT(self, n):
+        """
+        :type n: int
+        :rtype: List[Optional[TreeNode]]
+        """
+        dp = {}
+      
+        def dfs(val):
+            if not (val % 2):
+                return []
+            
+            if val == 1:
+                return [TreeNode(0)]
+
+            res = []
+            for left in range(1,val,2):
+                
+                right = val - left - 1
+                for left_sub in dfs(left):
+                    for right_sub in dfs(right):
+                        root = TreeNode(0)
+                        root.left = left_sub
+                        root.right = right_sub
+                        res.append(root)
+                        #dp[val] = root
+            return res
+        return dfs(n)
