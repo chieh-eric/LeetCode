43697b095b8e7809b9a9f9218a9f329d0f094444
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-06-29 00:24:33 +0900

    Time: 723 ms (10.53%), Space: 12.6 MB (94.74%) - LeetHub

---
+++ 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def numSubmat(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: int
+        """
+        m = len(mat)
+        n = len(mat[0])
+        
+        for i in range(m):
+            for j in range(n):
+                if i > 0 and mat[i][j] != 0:
+                    mat[i][j] += mat[i-1][j]
+        print(mat)
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] != 0:
+                    min_h = mat[i][j]
+                    for k in range(j, -1, -1):
+                        if mat[i][k] == 0:
+                            break
+                        min_h = min(min_h, mat[i][k])
+                        count += min_h
+        return count
+        # [[0, 1, 1, 0], 
+        # [0, 2, 2, 1], 
+        # [1, 3, 3, 0]]
