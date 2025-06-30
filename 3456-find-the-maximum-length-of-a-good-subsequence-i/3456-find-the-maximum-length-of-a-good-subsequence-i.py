a7c89531f0bd4a09de81d292d4d526c1d4c56504
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-06-30 16:29:54 +0900

    Time: 7191 ms (11.11%), Space: 12.7 MB (77.78%) - LeetHub

--- 3456-find-the-maximum-length-of-a-good-subsequence-i/3456-find-the-maximum-length-of-a-good-subsequence-i.py
+++ 3456-find-the-maximum-length-of-a-good-subsequence-i/3456-find-the-maximum-length-of-a-good-subsequence-i.py
@@ -5,26 +5,18 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        # helper(i,k) => Start from index i, can perform k operation. Retrun the maximum length
-        ans = 0
+        # dp[i][j] means ending at position i, with k operation, the maximum length of good subsequence
+        # Recursive form: dp[i][j] = max(dp[i-])
         n = len(nums)
-        dp = {}
-
-        def helper(i,k):
-            if i >= n or k < 0:
-                return 0
-            if (i,k) in dp:
-                return dp[(i,k)]
-
-            mx = 0
-            for j in range(i+1,n):
-                if nums[j] == nums[i]:
-                    mx = max(mx, helper(j,k))
-                else:
-                    mx = max(mx,helper(j,k-1))
-            dp[(i,k)] = mx + 1
-            return mx + 1
+        dp = [[1]*(k+1) for _ in range(n)]
+        
+        ans = 1 
         for i in range(n):
-            ans = max(ans,helper(i,k))
+            for j in range(i):
+                for t in range(k+1):
+                    if nums[i] == nums[j]:
+                        dp[i][t] = max(dp[i][t], dp[j][t]+1)
+                    elif t > 0:
+                        dp[i][t] = max(dp[i][t], dp[j][t-1]+1)
+            ans = max(ans,max(dp[i]))
         return ans
-       
