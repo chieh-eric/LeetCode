e49843f3e1f9a2b5ed2e13462a5dddd481d99bcd
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-07-03 22:29:40 +0900

    Time: 2976 ms (25.92%), Space: 12.5 MB (27.78%) - LeetHub

---
+++ 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def distributeCookies(self, cookies, k):
+        """
+        :type cookies: List[int]
+        :type k: int
+        :rtype: int
+        """
+        children = [0]*k
+        n = len(cookies)
+        self.min_unfair = float('inf')
+
+        def backtrack(i):
+            if i == n:
+                self.min_unfair = min(self.min_unfair,max(children))
+                return
+
+            for j in range(k):
+                children[j] += cookies[i]
+
+                if children[j] < self.min_unfair:
+                    backtrack(i+1)
+
+                children[j] -= cookies[i]
+        backtrack(0)
+        return self.min_unfair          
+                
+
+
+                
+        
