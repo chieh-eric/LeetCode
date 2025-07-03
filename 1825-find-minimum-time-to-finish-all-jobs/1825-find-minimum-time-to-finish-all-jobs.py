27fa19ddf779361b4b0aa7c9fd8bcbd910eb4484
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-07-04 00:05:03 +0900

    Time: 3127 ms (27.72%), Space: 12.3 MB (81.19%) - LeetHub

---
+++ 1825-find-minimum-time-to-finish-all-jobs/1825-find-minimum-time-to-finish-all-jobs.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def minimumTimeRequired(self, jobs, k):
+        """
+        :type jobs: List[int]
+        :type k: int
+        :rtype: int
+        """
+        worker = [0]*k
+        self.min_time = float('inf')
+        n = len(jobs)
+
+        def backtrack(i):
+            if i == n:
+                self.min_time = min(self.min_time, max(worker))
+                return
+            
+            for j in range(k):
+                worker[j] += jobs[i]
+                if self.min_time > worker[j]:
+                    backtrack(i+1)
+                
+                worker[j] -= jobs[i]
+                if worker[j] == 0:
+                    break
+        backtrack(0)
+        return self.min_time
