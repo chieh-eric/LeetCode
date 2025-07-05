f3065ea087a2975ed029081f88cf09b589af3740
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-07-06 01:32:45 +0900

    Time: 960 ms (6.67%), Space: 165.7 MB (6.67%) - LeetHub

---
+++ 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def longestCycle(self, edges):
+        """
+        :type edges: List[int]
+        :rtype: int
+        """
+        graph = {}
+        for i, to in enumerate(edges):
+            if to != -1:
+                graph[i] = to
+        n = len(edges)
+        self.max_length = -1
+
+        stepMap = {}
+        def dfs(node,step):
+            if node in stepMap:
+                if start_node == stepMap[node][1]:
+                    self.max_length = max(self.max_length, step - stepMap[node][0])
+                return
+            
+            stepMap[node] = (step, start_node) 
+            if node in graph:
+                dfs(graph[node],step+1)
+
+        
+        for i in range(n):
+            start_node = i
+            dfs(i,0)
+        return self.max_length
+
+        #0 -> 1 -> 2 -> 0
+
+        #3 -> 4 ->5 -> 6 -> 3
+
+        #7 -> 8 -> 9 -> 7
