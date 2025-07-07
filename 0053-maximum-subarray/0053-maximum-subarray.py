3b69e40d2cba7b87c41ff64ea126a3af937bd739
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-07-08 03:16:05 +0900

    Time: 111 ms (5.92%), Space: 21.3 MB (45.76%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,7 +1,14 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        dp = [0]*len(nums)
-        dp[0] = nums[0]
-        for i in range(1,len(nums),1):
-            dp[i] = max(dp[i-1] + nums[i], nums[i])
-        return max(dp)
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        prefix = 0
+        min_val = 0
+        max_val = -float('inf')
+        for i in range(len(nums)):
+            min_val = min(min_val, prefix)
+            prefix += nums[i]
+            max_val = max(max_val, prefix-min_val)
+        return max_val
