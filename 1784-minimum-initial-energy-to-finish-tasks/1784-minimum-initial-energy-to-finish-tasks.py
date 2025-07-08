d08f170b854bd959b96a06fb99ac6a4134754acf
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-07-08 19:11:34 +0900

    Time: 630 ms (0%), Space: 49.7 MB (0%) - LeetHub

---
+++ 1784-minimum-initial-energy-to-finish-tasks/1784-minimum-initial-energy-to-finish-tasks.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def minimumEffort(self, tasks):
+        """
+        :type tasks: List[List[int]]
+        :rtype: int
+        """
+        right = sum(task[1] for task in tasks)
+        left = 0
+
+        tasks.sort(key = lambda x:(x[1]-x[0],x[1]),reverse = True)
+        
+        def finish(val):
+            #print(tasks)
+            for task in tasks:
+                if val >= task[1]:
+                    val -= task[0]
+                else:
+                    return False
+            return True
+        
+        while left < right:
+            mid = (left+right) // 2
+            res = finish(mid)
+
+            if not res:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+            
