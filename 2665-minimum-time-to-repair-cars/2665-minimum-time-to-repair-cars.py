94c6b1ab0e53156a46636fbd3b72b5ca499c3818
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-07-08 21:46:44 +0900

    Time: 1698 ms (13.33%), Space: 14.8 MB (86.67%) - LeetHub

---
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def repairCars(self, ranks, cars):
+        """
+        :type ranks: List[int]
+        :type cars: int
+        :rtype: int
+        """
+        left = 1
+        right = max(ranks)*cars*cars
+
+        def finish(time):
+            car = 0
+            for m in ranks:
+                car += int((time/m)**0.5) 
+            return car
+
+        while left < right:
+            mid = (left+right) // 2
+            valid = finish(mid)
+
+            if valid < cars:
+                left = mid + 1
+            else:
+                right = mid
+        return left
