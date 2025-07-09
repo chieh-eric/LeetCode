f9a61cd63932f6f7343cdbe01d387cd2f5b33483
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-07-09 17:00:55 +0900

    Time: 228 ms (89.47%), Space: 30.4 MB (68.42%) - LeetHub

--- 2780-make-costs-of-paths-equal-in-a-binary-tree/2780-make-costs-of-paths-equal-in-a-binary-tree.py
+++ 2780-make-costs-of-paths-equal-in-a-binary-tree/2780-make-costs-of-paths-equal-in-a-binary-tree.py
@@ -5,33 +5,15 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
-        l = len(cost)
-        self.max_path = 0
-        self.count = 0
+        self.ans = 0
 
-        def dfs(i,cur):
-            cur += cost[i-1]
-            if 2*i > l and 2*i + 1 > l:
-                self.max_path = max(self.max_path,cur)
-                return
+        def dfs(i):
+            if 2*i > n:
+                return cost[i-1]
             
-            dfs(2*i,cur)
-            dfs(2*i+1,cur)
-
-        dfs(1,0)
-
-        def find(i,cur):
-            cur += cost[i-1]
-            if 2*i > l and 2*i + 1 > l:
-                return self.max_path - cur
-            
-            left = find(2*i,cur)
-            right = find(2*i+1,cur)
-            min_val = min(left,right)
-            self.count += (left+right) - 2*min_val
-            return min_val
-                                                  
-        find(1,0)
-        #print(self.max_path)
-        #print(self.count)
-        return self.count
+            left = dfs(2*i)
+            right = dfs(2*i+1)
+            self.ans += abs(left-right)
+            return cost[i-1] + max(left,right)
+        dfs(1)
+        return self.ans
