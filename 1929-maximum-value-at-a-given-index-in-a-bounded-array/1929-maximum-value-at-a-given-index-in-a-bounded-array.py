9d50be7624654278396abd6b19ce558846336ed3
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-07-10 14:54:17 +0800

    Time: 4 ms (56%), Space: 12.5 MB (52%) - LeetHub

--- 1929-maximum-value-at-a-given-index-in-a-bounded-array/1929-maximum-value-at-a-given-index-in-a-bounded-array.py
+++ 1929-maximum-value-at-a-given-index-in-a-bounded-array/1929-maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -9,18 +9,12 @@ class Solution(object):
 
         def calculate(val):
             min_left = min(index, val -1)
-            #print(min_left)
             min_left_val = (val+val-min_left)*(min_left+1)//2 - val
-            #print(min_left_val)
 
             min_right = min(n-index-1, val-1)
-            #print(min_right)
             min_right_val = (val+val-min_right)*(min_right+1)//2 - val
-            #print(min_right_val)
 
             return min_left_val+min_right_val+val + 1*(index-min_left) + 1*(n-index-1-min_right)
-        #print(calculate(2))
-        # 2 3 2 1 0 0
         l = maxSum // n
         r = maxSum - l
         
