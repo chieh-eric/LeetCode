48634be847e2e1057581fb1bde0d150fd736a909
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-07-12 21:17:59 +0800

    Time: 951 ms (5%), Space: 31.9 MB (35%) - LeetHub

---
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.py
@@ -0,0 +1,35 @@
+import bisect
+class Solution(object):
+    def avoidFlood(self, rains):
+        """
+        :type rains: List[int]
+        :rtype: List[int]
+        """
+        n = len(rains)
+        res = [-1]*n
+        full = {}
+        dry = SortedList()
+
+        for i in range(n):
+            lake = rains[i]
+            if lake == 0:
+                dry.add(i)
+                continue
+            
+            else:
+                if lake in full:
+                    idx = bisect.bisect_right(dry,full[lake])
+                    if idx == len(dry):
+                        return []
+                    
+                    dry_day = dry[idx]
+                    res[dry_day] = lake
+                    dry.remove(dry_day)
+                full[lake] = i
+        while dry:
+            idx = dry.pop()
+            res[idx] = 1
+        return res
+
+
+                
