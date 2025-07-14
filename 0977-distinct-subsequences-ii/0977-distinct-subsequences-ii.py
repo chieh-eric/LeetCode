effa038e426dc6b074c1f01b86e5ec0edcfeca62
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-07-14 21:31:49 +0800

    Time: 9 ms (87.5%), Space: 12.7 MB (25%) - LeetHub

--- 0977-distinct-subsequences-ii/0977-distinct-subsequences-ii.py
+++ 0977-distinct-subsequences-ii/0977-distinct-subsequences-ii.py
@@ -6,12 +6,13 @@ class Solution(object):
         """
         mod = 10**9+7
         n = len(s)
-        dp = [0]*n
+        dp = [0]*(n+1)
         dp[0] = 1
-        for i in range(1,n):
-            for j in range(i):
-                if s[i] != s[j]:
-                    dp[i] += dp[j]
-            dp[i] += 1
-            dp[i] %= mod
-        return sum(dp) % mod
+        last = {}
+        for i in range(n):
+            ch = s[i]
+            dp[i+1] = (2*dp[i]) % mod
+            if s[i] in last:
+                dp[i+1] = (dp[i+1] - dp[last[s[i]]]) % mod
+            last[s[i]] = i
+        return (dp[n] - 1) % mod
