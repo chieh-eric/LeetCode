3a1124603c41118d901b6acc6a730d7c1ff5b839
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-07-17 12:30:14 +0800

    Time: 66 ms (18.18%), Space: 13.5 MB (36.36%) - LeetHub

--- 0963-three-equal-parts/0963-three-equal-parts.py
+++ 0963-three-equal-parts/0963-three-equal-parts.py
@@ -14,44 +14,21 @@ class Solution(object):
             return [0,n-1]
 
 
-        count_one = count[1] // 3
-        last_pointer  = -1
-        slide = Counter()
-        
-        for i in range(n-1,-1,-1):
-            slide[arr[i]] += 1
-            if slide[1] == count_one:
-                last_pointer = i
-                break
-        
-        first_pointer = -1
-        common = arr[last_pointer:]
-        #print(common)
-        m = len(common)
-        for i in range(n):
-            if  arr[i] == 1:
-                first_pointer = i
-                break
-        first_split = first_pointer + m - 1
+        one_pos = [i for i, bit in enumerate(arr) if bit == 1]
+        target = count[1] // 3
 
-        #print(arr[first_pointer:first_split+1])
-        if arr[first_pointer:first_split+1] != common:
-            return [-1,-1]
-        
-        second_pointer = -1
-        for i in range(first_split+1,n):
-            if arr[i] == 1:
-                second_pointer = i
-                break
-        second_split = second_pointer + m
+        i1 = one_pos[0]
+        i2 = one_pos[target]
+        i3 = one_pos[target*2]
 
-        #print(second_pointer)
-        #print(second_split)
-        #print(arr[second_pointer:second_split])
-        if arr[second_pointer:second_split] != common:
-            return [-1,-1]
+        while i3 < n:
+            if arr[i1] != arr[i2] or arr[i2] != arr[i3]:
+                return [-1,-1]
+            i1 += 1
+            i2 += 1
+            i3 += 1
         
-        return [first_split,second_split]
+        return [i1-1,i2]
 
 
             
