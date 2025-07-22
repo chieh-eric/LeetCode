4320010766162354c983688926e8c0e33da90c69
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-07-22 21:58:36 +0800

    Time: 1414 ms (70%), Space: 12.7 MB (30%) - LeetHub

---
+++ 2810-collecting-chocolates/2810-collecting-chocolates.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def minCost(self, nums, x):
+        """
+        :type nums: List[int]
+        :type x: int
+        :rtype: int
+        """
+        # dp[i] means the minimum cost to select
+        n = len(nums)
+        min_cost = min(nums)
+        min_index = nums.index(min_cost)
+        cur = nums[::]
+        prev = sum(cur)
+        for rotate in range(1,n+1):
+            for i in range(n-1,-1,-1):
+               
+                cur[i] = min(cur[i],nums[(i-rotate)%n])
+                #print(cur)
+            if prev < sum(cur)+x*rotate:
+                return prev
+            prev = sum(cur)+x*rotate
+            #print(prev)
+        #print(cur)
