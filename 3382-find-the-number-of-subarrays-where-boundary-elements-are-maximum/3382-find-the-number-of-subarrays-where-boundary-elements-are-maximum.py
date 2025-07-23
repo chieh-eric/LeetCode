1992c3911f5b3b9ba8afb0bf2c5fdc10a72707c4
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-07-23 11:10:32 +0800

    Time: 2665 ms (0%), Space: 24.4 MB (100%) - LeetHub

---
+++ 3382-find-the-number-of-subarrays-where-boundary-elements-are-maximum/3382-find-the-number-of-subarrays-where-boundary-elements-are-maximum.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+class Solution(object):
+    def numberOfSubarrays(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def calculate(count):
+         
+            return count*(count+1)//2
+        
+        res = 0
+        dic = defaultdict(int)
+        stack = []
+        for num in nums:
+            while stack and num > stack[-1]:
+                print(stack[-1])
+                res += calculate(dic[stack[-1]])
+                print(res)
+                dic[stack[-1]] = 0
+                stack.pop()
+            stack.append(num)
+            dic[num] += 1
+        for key in dic:
+            res += calculate(dic[key])
+        return res
