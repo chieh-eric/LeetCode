93f2da3f6b8ff5db6d52e4d0b44abcccc5163c43
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-07-28 14:24:51 +0800

    Time: 561 ms (100%), Space: 72.7 MB (14.29%) - LeetHub

---
+++ 3386-find-edges-in-shortest-paths/3386-find-edges-in-shortest-paths.py
@@ -0,0 +1,54 @@
+from collections import defaultdict
+import heapq
+class Solution(object):
+    def findAnswer(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: List[bool]
+        """
+        graph = defaultdict(list)
+        for index,(i, j, w) in enumerate(edges):
+            graph[i].append((j,w,index))
+            graph[j].append((i,w,index))
+
+        m = len(edges)
+        res = [False]*m
+        heap = [(0,0,-1)]
+        shortest = float('inf')
+        visited = set()
+        while heap:
+            step, node, i = heapq.heappop(heap)
+            if node == n-1:
+                shortest = step
+                break
+            visited.add(node)
+            for nei, w, idx in graph[node]:
+                if nei not in visited:
+                    heapq.heappush(heap,(step+w,nei,idx))
+
+        if shortest == float('inf'):
+            return res
+        def dfs(node,parent,length):
+            if length > shortest:
+                return False
+
+            if node == n - 1 and length == shortest:
+                return True
+
+            valid = False
+            for nei, w, idx in graph[node]:
+                if nei == parent:
+                    continue
+                if(dfs(nei,node,length+w)):
+                    res[idx] = True
+                    valid = True
+            return valid
+        #print(shortest)     
+        dfs(0,None,0)
+        return res
+
+        # 2 0 1
+        #   4
+        #   5
+        #   3
