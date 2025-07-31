47d94982af0ab312d5344489ba78689a6b1c2c70
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-07-31 18:44:15 +0800

    Time: 42 ms (92.31%), Space: 12.5 MB (96.7%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.py
+++ 1129-longest-string-chain/1129-longest-string-chain.py
@@ -6,25 +6,11 @@ class Solution(object):
         """
         m = len(words)
         words.sort(key=lambda x:len(x))
-        def calculate(source,target):
-            if len(source) + 1 != len(target):
-                return 0
-            
-            index = 0
-            n = len(source)
-            for i in range(len(target)):
-                if index < n and target[i] == source[index]:
-                    index += 1
-            return index == n
-
-        dp = [1]*m
-        for i in range(1, m):
-            count = 1
-            for j in range(i-1,-1,-1):
-                if len(words[j]) + 1 < len(words[i]):
-                    break
-                if calculate(words[j],words[i]):
-                    count = max(count,dp[j]+1)
-            dp[i] = count
-        ##print(dp)
-        return max(dp)
+        dic = {}
+        for word in words:
+            dic[word] = 1
+            for i in range(len(word)):
+                cut_word = word[:i] + word[i+1:]
+                if cut_word in dic:
+                    dic[word] = max(dic[word], dic[cut_word] + 1)
+        return max(dic.values())
