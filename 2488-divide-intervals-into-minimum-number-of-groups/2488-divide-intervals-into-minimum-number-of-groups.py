807df5bdcc4a451b99d1d72e5c898ff35b846d30
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-08-03 17:22:03 +0800

    Time: 864 ms (14.81%), Space: 76.3 MB (7.41%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.py
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.py
@@ -4,20 +4,13 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: int
         """
-        intervals.sort(key=lambda x:(x[0],-x[1]))
-        #print(intervals)
-        arr = SortedList()
-        for left, right in intervals:
-
-            if not arr:
-                arr.add(right)
-            else:
-                idx = arr.bisect_left(left)
-                
-                if idx == 0:
-                    arr.add(right)
-                else:
-                    arr.pop(idx-1)
-                    arr.add(right)
-            #print(arr)
-        return len(arr)
+        events = []
+        for i, j in intervals:
+            events.append((i,1))
+            events.append((j,-1))
+        cur = group = 0
+        events.sort(key=lambda x:(x[0],-x[1]))
+        for _, i in events:
+            cur += i
+            group = max(group, cur)
+        return group
