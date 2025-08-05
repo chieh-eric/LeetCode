0a4c0792e45172e5fcafd65e73f1840ff0422428
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-08-05 23:48:06 +0800

    Time: 105 ms (75%), Space: 16.2 MB (12.5%) - LeetHub

--- 2817-minimum-cost-to-make-all-characters-equal/2817-minimum-cost-to-make-all-characters-equal.py
+++ 2817-minimum-cost-to-make-all-characters-equal/2817-minimum-cost-to-make-all-characters-equal.py
@@ -4,25 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # Prefix -> Means in index i, the total cost of slipping(not include flip current index)
+        # 1 110101
+        # 3 000101
+        # 6 000010
+        # 8 000001
+        # 9 000000
+        total = 0
         n = len(s)
-        prefix = [0]*n
-        suffix = [0]*n
-
         prev = s[0]
-        cost = 0
         for i in range(1,n):
-            if prev != s[i]:
-                cost += i
-            prefix[i] = cost
+            if s[i] != prev:
+                total += min(i,n-i)
             prev = s[i]
-
-        nxt = s[-1]
-        cost = 0
-        for i in range(n-2,-1,-1):
-            if nxt != s[i]:
-                cost += (n-i-1)
-            suffix[i] = cost
-            nxt = s[i]
-
-        return min(x+y for x, y in zip(prefix,suffix))
+        return total
