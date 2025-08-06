bff2a570474b95d56f93df317796080ca3b17e26
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-08-07 01:20:51 +0800

    Time: 3364 ms (22.81%), Space: 12.6 MB (66.83%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -4,22 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # memo = {}
-        # def dfs(val):
-        #     if (val**0.5).is_integer():
-        #         memo[val] = 1
-        #         return 1
-
-        #     min_op = float('inf')
-        #     if val in memo:
-        #         return memo[val]
-            
-        #     for i in range(1,int(val**0.5)+1):
-        #         min_op = min(min_op, dfs(val-i**2) + 1)
-            
-        #     memo[val] = min_op
-        #     return min_op
-        # return dfs(n)
+      
 
         dp = [float('inf')]*(n+1)
         dp[0] = 0
@@ -28,5 +13,4 @@ class Solution(object):
             while j*j <= i:
                 dp[i] = min(dp[i],dp[i-(j*j)] + 1)
                 j += 1
-        #print(dp)
         return dp[n]
