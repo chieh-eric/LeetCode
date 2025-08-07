a93febe5b0589e141dbe136c6f94aac7166836bd
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-08-07 14:45:28 +0800

    Time: 135 ms (85.71%), Space: 20.5 MB (100%) - LeetHub

--- 2893-visit-array-positions-to-maximize-score/2893-visit-array-positions-to-maximize-score.py
+++ 2893-visit-array-positions-to-maximize-score/2893-visit-array-positions-to-maximize-score.py
@@ -5,22 +5,20 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-       # 2 2 8 (8,4) 
-       # 0 3 4 4 13
+        max_even = max_odd = 0
         if nums[0] % 2 == 0:
             max_even = nums[0]
-            max_odd = float('-inf')  
+            max_odd = nums[0] - x
         else:
+            max_even = nums[0] - x
             max_odd = nums[0]
-            max_even = float('-inf')
-            
+        
         n = len(nums)
         for i in range(1,n):
             if nums[i] % 2 == 0:
-                max_even = max(max_even + nums[i], max_odd + nums[i] - x)
+                max_even = max(max_even, max_even+nums[i], max_odd + nums[i] - x)
             else:
-                max_odd = max(max_odd + nums[i], max_even + nums[i] - x)
-
-
-
-        return max(max_odd,max_even)
+                max_odd = max(max_odd, max_odd+nums[i], max_even + nums[i] - x)
+           
+        return max(max_even, max_odd)
+       
