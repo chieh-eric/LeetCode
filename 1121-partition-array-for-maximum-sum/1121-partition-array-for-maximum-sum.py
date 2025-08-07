54df6b9a2fe180baff59163ebd1c871436fe2448
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-08-07 15:31:11 +0800

    Time: 272 ms (51.04%), Space: 12.6 MB (61.46%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.py
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.py
@@ -7,13 +7,13 @@ class Solution(object):
         """
         n = len(arr)
         dp = [0]*(n+1)
-        dp[1] = arr[0]
 
-        for i in range(2,n+1):
-
+        for i in range(1,n+1):
+            max_val = arr[i-1]
             for j in range(1,k+1):
                 if i - j < 0:
                     break
-                dp[i] = max(dp[i], dp[i-j] + max(arr[i-j:i])*j)
+                max_val = max(max_val, arr[i-j])
+                dp[i] = max(dp[i], dp[i-j] + max_val*j)
         #print(dp)
         return dp[n]
