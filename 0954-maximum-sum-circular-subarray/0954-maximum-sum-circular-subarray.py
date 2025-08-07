7ad55d3baf8d8272d1bd495957e6e14811e6f773
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-08-07 14:27:27 +0800

    Time: 105 ms (55.5%), Space: 15 MB (81.61%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.py
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.py
@@ -4,24 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
-        max_sum = min_sum = nums[0]
-        cur = nums[0]
+        max_val = min_val = cur_max = cur_min = nums[0]
         n = len(nums)
+        for i in range(1,n):
+            cur_max = max(nums[i], cur_max + nums[i])
+            cur_min = min(nums[i], cur_min + nums[i])
+
+            max_val = max(max_val,cur_max)
+            min_val = min(min_val,cur_min)
+        
         total = sum(nums)
-        # Find original largest
-        for i in range(1,n):
-            cur = max(cur+nums[i],nums[i])
-            max_sum = max(max_sum,cur)
-
-        if max(nums) < 0:
-            return max_sum
-
-        # Find the circular largest
-        cur = nums[0]
-        for i in range(1,n):
-            cur = min(cur+nums[i],nums[i])
-            min_sum = min(min_sum,cur)
-
-        return max(max_sum, total-min_sum)
-       
+        if min_val == total:
+            return max_val
+        
+        return max(max_val, total-min_val)
+        
