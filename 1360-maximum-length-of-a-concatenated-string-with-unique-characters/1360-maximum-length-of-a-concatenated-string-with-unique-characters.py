4282948e1e6b0ee9d3b18603227eb456a8b9b15c
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-08-09 00:46:42 +0800

    Time: 283 ms (21.82%), Space: 12.5 MB (69.09%) - LeetHub

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -4,28 +4,34 @@ class Solution(object):
         :type arr: List[str]
         :rtype: int
         """
-        cur_mask = 0
-        self.count = 0
-        n = len(arr)
-        def dfs(mask,index):
-            self.count = max(self.count,bin(mask).count('1'))
-            if index >= n :
-                return
         
+        self.max_len = 0
+        new_arr = []
+        for word in arr:
+            c = Counter()
+            good = True
+            for ch in word:
+                if ch in c:
+                    good = False
+                    break
+                c[ch] += 1
+            if good:
+                new_arr.append(word)
+        arr = new_arr
+        n = len(arr)
+        def backtrack(i,count):
+            if i == n:
+                self.max_len = max(self.max_len, sum(count.values()))
+                return
+            add = True
+            backtrack(i+1,count)
+            temp = count.copy()
+            for ch in arr[i]:
+                if ch in count:
+                    add = False
+                temp[ch] += 1
             
-            for i in range(index,n):
-                new_mask = 0
-                valid = True
-                for ch in arr[i]:
-                    bit = (1 << (ord(ch)-ord('a')))
-                    if bit & new_mask > 0:
-                        valid = False
-                        break
-                    new_mask |= bit
-
-                if not valid or (new_mask & mask) > 0:
-                    continue
-                new_mask = new_mask | mask
-                dfs(new_mask,i+1)
-        dfs(0,0)
-        return self.count
+            if add:
+                backtrack(i+1,temp)
+        backtrack(0,Counter())
+        return self.max_len
