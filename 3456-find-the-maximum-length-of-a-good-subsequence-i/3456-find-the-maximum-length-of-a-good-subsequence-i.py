293b7efa69f21127b8aaac0c5ba0b3e2ff72ea81
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-08-09 15:26:54 +0800

    Time: 61 ms (87.5%), Space: 12.6 MB (81.25%) - LeetHub

--- 3456-find-the-maximum-length-of-a-good-subsequence-i/3456-find-the-maximum-length-of-a-good-subsequence-i.py
+++ 3456-find-the-maximum-length-of-a-good-subsequence-i/3456-find-the-maximum-length-of-a-good-subsequence-i.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 class Solution(object):
     def maximumLength(self, nums, k):
         """
@@ -5,18 +6,20 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        # dp[i][j] means ending at position i, with k operation, the maximum length of good subsequence
-        # Recursive form: dp[i][j] = max(dp[i-])
         n = len(nums)
-        dp = [[1]*(k+1) for _ in range(n)]
-        
-        ans = 1 
-        for i in range(n):
-            for j in range(i):
-                for t in range(k+1):
-                    if nums[i] == nums[j]:
-                        dp[i][t] = max(dp[i][t], dp[j][t]+1)
-                    elif t > 0:
-                        dp[i][t] = max(dp[i][t], dp[j][t-1]+1)
-            ans = max(ans,max(dp[i]))
-        return ans
+        # dp is a dictionary, key as the interger, value as ()
+        dp = defaultdict(lambda: [0]*(k+1))
+        best = [0]*(k+1)
+
+        for x in nums:
+            arr = dp[x]
+            new = arr[:]
+
+            for t in range(k, -1, -1):
+                same = arr[t] + 1
+                change = best[t-1] + 1 if t > 0 else 0
+                new[t] = max(arr[t],same,change)
+            dp[x] = new
+            for i in range(k+1):
+                best[i] = max(best[i],new[i])
+        return max(best)
