7a92719cee813065a6bc704145b37ad33fbb1af9
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-08-11 14:27:38 +0800

    Time: 19 ms (71.43%), Space: 12.5 MB (38.1%) - LeetHub

--- 3613-maximize-amount-after-two-days-of-conversions/3613-maximize-amount-after-two-days-of-conversions.py
+++ 3613-maximize-amount-after-two-days-of-conversions/3613-maximize-amount-after-two-days-of-conversions.py
@@ -9,42 +9,43 @@ class Solution(object):
         :type rates2: List[float]
         :rtype: float
         """
-        day1_graph = defaultdict(list)
-        day2_graph = defaultdict(list)
-        intermediate = defaultdict(float)
-        visited =  defaultdict(float)
-        
-        for i, (start, end) in enumerate(pairs1):
-            day1_graph[start].append((end,rates1[i]))
-            day1_graph[end].append((start,1/rates1[i]))
-        
-        for i, (start, end) in enumerate(pairs2):
-            day2_graph[start].append((end,rates2[i]))
-            day2_graph[end].append((start,1/rates2[i]))
-            
+        graph1 = defaultdict(list)
+        for index, (i, j) in enumerate(pairs1):
+            graph1[i].append((j,rates1[index]))
+            graph1[j].append((i,1/float(rates1[index])))
+        queue1 = deque()
+        queue1.append((initialCurrency,1))
+        day1 = defaultdict(float)
 
-        queue = deque()
-        queue.append((initialCurrency,1.0))
-        res = 0
-        while queue:
-            currency, rate = queue.popleft()
-            for neighbor, amt in day1_graph[currency]:
-                new_amt = rate*amt
-                if new_amt > intermediate[neighbor]:
-                    intermediate[neighbor] = new_amt
-                    queue.append((neighbor,new_amt))
+        while queue1:
+            nxt, cur = queue1.popleft()
+            for nei, rate in graph1[nxt]:
+                if cur*rate > day1[nei]:
+                    queue1.append((nei, cur*rate))
+                    day1[nei] = (cur*rate)
+        #print(day1)
 
-        for currency, rate in intermediate.items():
-            queue.append((currency,rate))
-            while queue:
-                cur, amt = queue.popleft()
-                if cur == initialCurrency:
-                    res = max(res,amt)
-                for neighbor, val in day2_graph[cur]:
-                    new_amt = amt*val
-                    if new_amt > visited[neighbor]:
-                        visited[neighbor] = new_amt
-                        queue.append((neighbor,new_amt))
+
+
+        graph2 = defaultdict(list)
+        for index, (i, j) in enumerate(pairs2):
+            graph2[i].append((j,rates2[index]))
+            graph2[j].append((i,1/float(rates2[index])))
+
+        day2 = defaultdict(float)
+        queue2 = deque()
+        res = 1
+        for start, init in day1.items():
+            queue2.append((start,init))
+
+            while queue2:
+                nxt, cur = queue2.popleft()
+                if nxt == initialCurrency:
+                    res = max(res, cur)
+                for nei, rate in graph2[nxt]:
+                    if cur*rate > day2[nei]:
+                        queue2.append((nei, cur*rate))
+                        day2[nei] = (cur*rate)
+        #print(day2)
 
         return res
-
