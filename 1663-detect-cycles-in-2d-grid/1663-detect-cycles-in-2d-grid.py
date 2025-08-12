4e97aad7e8f9b2c789fc0bafe998d51466251c5d
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-08-13 02:21:32 +0800

    Time: 648 ms (69.57%), Space: 206.4 MB (60.87%) - LeetHub

--- 1663-detect-cycles-in-2d-grid/1663-detect-cycles-in-2d-grid.py
+++ 1663-detect-cycles-in-2d-grid/1663-detect-cycles-in-2d-grid.py
@@ -4,35 +4,47 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: bool
         """
-
         m = len(grid)
         n = len(grid[0])
-        direction = [(0,1),(1,0),(-1,0),(0,-1)]
-        def dfs(i,j,length,prev_i,prev_j):
-         
-            if (i,j) in visited:
+
+        visited = [[0]*n for _ in range(m)]
+        direction = [(0,1),(1,0),(0,-1),(-1,0)]
+        def dfs(i,j,parent,letter):
+            if visited[i][j] == 2:
+                return False
+            
+            if visited[i][j] == 1:
                 return True
 
-            visited.add((i,j))
-            val = grid[i][j]
+            visited[i][j] = 1
+
             for dx, dy in direction:
-                nx = dx + i
-                ny = dy + j
-                if 0 <= nx < m and 0 <= ny < n and val == grid[nx][ny] and (nx != prev_i or ny != prev_j):
-                    if dfs(nx,ny,length+1,i,j):
-                        return True
-            return False 
-            
-        visited = set()
+                nx = i + dx
+                ny = j + dy
+                if 0 <= nx < m and 0 <= ny < n:
+                    if parent and nx == parent[0] and ny == parent[1]:
+                        continue
+                    
+                    if visited[nx][ny] != 2 and grid[nx][ny] == letter:
+                        #print(grid[i][j])
+                        #print(letter)
+                        if dfs(nx,ny,(i,j),letter):
+                            return True
+            visited[i][j] = 2
+            return False
+        
         for i in range(m):
             for j in range(n):
-                if (i,j) not in visited:
-                    if dfs(i,j,1,None,None):
+                if visited[i][j] == 0:
+                    #print(i,j)
+                    #print(visited)
+                    
+                    if dfs(i,j,None,grid[i][j]):
                         return True
         return False
 
-        # c a d
-        # a a a
-        # a a d
-        # a c d
-        # a b c
+        # ["f","a","a","c","b"],
+        # ["e","a","a","e","c"],
+        # ["c","f","b","b","b"],
+        # ["c","e","a","b","e"],
+        # ["f","e","f","b","f"]]
