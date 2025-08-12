07241532321fc4b28ffffc8018765d306d76e642
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-08-12 21:41:53 +0800

    Time: 15 ms (30.67%), Space: 12.6 MB (34.67%) - LeetHub

--- 0787-sliding-puzzle/0787-sliding-puzzle.py
+++ 0787-sliding-puzzle/0787-sliding-puzzle.py
@@ -4,38 +4,26 @@ class Solution(object):
         :type board: List[List[int]]
         :rtype: int
         """
+        direction = {0:[1,3], 1:[0,2,4], 2:[1,5], 3:[0,4], 4:[1,3,5], 5:[2,4]}
         queue = deque()
-        goal =  ["1","2","3","4","5","0"]
-        transition = {
-            0:[1,3],
-            1:[0,2,4],
-            2:[1,5],
-            3:[0,4],
-            4:[1,3,5],
-            5:[2,4]
-        }
-
-        state = []
-        for i in range(len(board)):
-            for j in range(len(board[i])):
-                state.append(str(board[i][j]))
+        queue.append((["1","2","3","4","5","0"],0))
+        valid = {}
         
-        queue = deque()
-        queue.append((state,0))
-        visited = set()
         while queue:
-            s, op = queue.popleft()
-            zero_index = s.index("0")
+            state, step = queue.popleft()
+            index = state.index("0")
+            if "".join(state) in valid:
+                continue
+            valid["".join(state)] = step
+            for nei in direction[index]:
+                temp = state[::]
+                temp[index], temp[nei] = temp[nei], temp[index]
+                if "".join(temp) not in valid:
+                    queue.append((temp,step+1))
+        target = ""
+        for row in board:
+            for element in row:
+                target += str(element)
+        return valid[target] if target in valid else -1
+
         
-            if "".join(s) == "".join(goal):
-                return op
-        
-            visited.add("".join(s))
-            for nxt in transition[zero_index]:
-                new_state = s[::]
-                new_state[zero_index] = new_state[nxt]
-                new_state[nxt] = "0"
-                if "".join(new_state) not in visited:
-                    queue.append((new_state,op+1))
-        return -1
-            
