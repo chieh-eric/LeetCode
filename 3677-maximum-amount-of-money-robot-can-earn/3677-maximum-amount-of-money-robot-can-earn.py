aed35e26ca6c32047e32d50b8669f7c9c1a6e5ac
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-08-13 22:51:17 +0800

    Time: 4837 ms (71.43%), Space: 70.6 MB (42.86%) - LeetHub

--- 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.py
+++ 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.py
@@ -4,34 +4,37 @@ class Solution(object):
         :type coins: List[List[int]]
         :rtype: int
         """
-        m = len(coins)
-        n = len(coins[0])
-        dp = [[[-float('inf')]*3 for _ in range(n)]for _ in range(m)]
+        n = len(coins)
+        m = len(coins[0])
 
-        if coins[0][0] > 0:
-            for k in range(3):
-                dp[0][0][k] = coins[0][0]
+        dp = [[[-float('inf')]*3 for _ in range(m)] for _ in range(n)]
+        #print(dp)
+        if coins[0][0] >= 0:
+            for i in range(3):
+                dp[0][0][i] = coins[0][0]
         else:
             dp[0][0][0] = coins[0][0]
-            dp[0][0][1] = 0
+            for i in range(1,3):
+                dp[0][0][i] = 0
 
-        for i in range(m):
-            for j in range(n):
+
+        for i in range(n):
+            for j in range(m):
+                val = coins[i][j]
                 for k in range(3):
-                    val = coins[i][j]
-                    if i > 0:
-                        if val > 0:
-                            dp[i][j][k] = max(dp[i][j][k],dp[i-1][j][k]+val)
-                        else:
+                    if coins[i][j] >= 0:
+                        if i > 0:
+                            dp[i][j][k] = max(dp[i][j][k], dp[i-1][j][k] + val)
+                        if j > 0:
+                            dp[i][j][k] = max(dp[i][j][k], dp[i][j-1][k] + val)
+                    else:
+                        if i > 0:
                             if k > 0:
-                                dp[i][j][k] = max(dp[i][j][k],dp[i-1][j][k-1])
-                            dp[i][j][k] = max(dp[i][j][k],dp[i-1][j][k]+val)
-                    if j > 0:
-                        if val > 0:
-                            dp[i][j][k] = max(dp[i][j][k],dp[i][j-1][k]+val)
-                        else:
+                                dp[i][j][k] = max(dp[i][j][k], dp[i-1][j][k-1])
+                            dp[i][j][k] = max(dp[i][j][k], dp[i-1][j][k] + val)
+                        if j > 0:
                             if k > 0:
-                                dp[i][j][k] = max(dp[i][j][k],dp[i][j-1][k-1])
-                            dp[i][j][k] = max(dp[i][j][k],dp[i][j-1][k]+val)
-                            
-        return max(dp[m-1][n-1][0],dp[m-1][n-1][1],dp[m-1][n-1][2])
+                                dp[i][j][k] = max(dp[i][j][k], dp[i][j-1][k-1])
+                            dp[i][j][k] = max(dp[i][j][k], dp[i][j-1][k] + val)
+        return max(dp[-1][-1])
+                   
