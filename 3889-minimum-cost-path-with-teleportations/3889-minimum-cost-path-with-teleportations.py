2ca910efb849ad27d841dd4ce40b8da4712ea0cf
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-08-17 15:00:55 +0800

    Time: 8056 ms (87.5%), Space: 14.6 MB (62.5%) - LeetHub

--- 3889-minimum-cost-path-with-teleportations/3889-minimum-cost-path-with-teleportations.py
+++ 3889-minimum-cost-path-with-teleportations/3889-minimum-cost-path-with-teleportations.py
@@ -1,5 +1,4 @@
 from collections import defaultdict
-import heapq
 class Solution(object):
     def minCost(self, grid, k):
         """
@@ -9,41 +8,38 @@ class Solution(object):
         """
         m = len(grid)
         n = len(grid[0])
-        visited = [[[False]*(k+1) for _ in range(n)]for _ in range(m)]
-        teleport = defaultdict(list)
-        vals = []
-        for i in range(m):
-            for j in range(n):
-                vals.append((grid[i][j],i,j))
-        vals.sort()
+        
+        def forward(dp):
 
-        for index, (val, i, j) in enumerate(vals):
-            idx = bisect.bisect_right(vals,(val,float('inf'),float('inf'))) - 1
-            for x in range(idx+1):
-                if (vals[x][1],vals[x][2]) == (i,j):
-                    continue
-                teleport[(i,j)].append((vals[x][1],vals[x][2]))
-                
-        #print(vals)
-        #print(teleport)
-        direction = [(0,1),(1,0)]
-        heap = []
-        heapq.heappush(heap,(0,0,0, k))
-        while heap:
-            cost, i, j, remain = heapq.heappop(heap)
-            if visited[i][j][remain]:
-                continue
-            if i == m - 1 and j == n - 1:
-                return cost
-            visited[i][j][remain] = True
-            for nx, ny in teleport[(i,j)]:
-                if not visited[nx][ny][remain] and remain > 0:
-                    heapq.heappush(heap,(cost,nx,ny,remain-1))
-
-            for dx, dy in direction:
-                nx = i + dx
-                ny = j + dy
-                if nx < m and ny < n and not visited[nx][ny][remain]:
-                    heapq.heappush(heap,(cost+grid[nx][ny],nx,ny,remain))
-      
+            for i in range(m):
+                for j in range(n):
+                    if i > 0:
+                        dp[i][j] = min(dp[i][j],dp[i-1][j] + grid[i][j])
                     
+                    if j > 0:
+                        dp[i][j] = min(dp[i][j], dp[i][j-1] + grid[i][j])
+
+        
+        dp = [[float('inf')]*n for _ in range(m)]
+        dp[0][0] = 0
+        forward(dp)
+     
+        for _ in range(k):
+            dp2 = [rows[:] for rows in dp]
+            vals = []
+            for i in range(m):
+                for j in range(n):
+                    vals.append((grid[i][j], dp2[i][j], i, j))
+            vals.sort(key= lambda x:(-x[0],x[1]))
+            min_val = 10**8
+            for val, _, i, j in vals:
+                if dp[i][j] < min_val:
+                    min_val = dp[i][j]
+                
+                if dp2[i][j] > min_val:
+                    dp2[i][j] = min_val
+            forward(dp2)
+            dp = dp2
+            
+        return dp[-1][-1]
+            
