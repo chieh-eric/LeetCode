ff6a7e102943ab632bbdd7b08c087feeb422f47b
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-08-19 23:05:12 +0800

    Time: 19 ms (90.84%), Space: 12.5 MB (74.05%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.py
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.py
@@ -5,22 +5,27 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-        total = 0
-        min_val = [[float('inf')]*n for _ in range(n)]
-        max_val = [[-float('inf')]*n for _ in range(n)]
+        inf = float('inf')
+        stack = []
+        res = 0
+        nums1 = [inf] + nums + [inf]
 
-        for i in range(n):
-            min_val[i][i] = max_val[i][i] = nums[i]
+        for i, num in enumerate(nums1):
 
-        for length in range(2, n + 1):
-            for i in range(n - length + 1):
-                j = i + length - 1
-                max_val[i][j] = max(max_val[i][j-1], nums[j])
-                min_val[i][j] = min(min_val[i][j-1], nums[j])
-        #print(max_val)
-        #print(min_val)
-        for i in range(n):
-            for j in range(n):
-                if  max_val[i][j] != -float('inf'):
-                    total += max_val[i][j] - min_val[i][j]
-        return total
+            while stack and nums1[stack[-1]] < nums1[i]:
+                j = stack.pop()
+                k = stack[-1]
+                res += nums1[j]*(i-j)*(j-k)
+            
+            stack.append(i)
+        
+        stack = []
+        nums1 = [-inf] + nums + [-inf]
+        for i, num in enumerate(nums1):
+
+            while stack and nums1[stack[-1]] > nums1[i]:
+                j = stack.pop()
+                k = stack[-1]
+                res -= nums1[j]*(i-j)*(j-k)
+            stack.append(i)
+        return res
