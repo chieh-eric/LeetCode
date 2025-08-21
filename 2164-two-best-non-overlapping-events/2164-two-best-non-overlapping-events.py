1b945a45bb4c5d0efa8c6cd768324eb1a34e0cdb
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-08-21 20:30:12 +0800

    Time: 452 ms (20%), Space: 50.7 MB (8.89%) - LeetHub

--- 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.py
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.py
@@ -1,27 +1,26 @@
-import bisect 
+import bisect
 class Solution(object):
     def maxTwoEvents(self, events):
         """
         :type events: List[List[int]]
         :rtype: int
         """
-        events.sort(key=lambda x:(x[1]))
-        n = len(events)
-        max_value_up_to = []
-        cur_max = 0
-        for start, end, val in events:
-            cur_max = max(cur_max, val)
-            max_value_up_to.append((end,cur_max))
-        max_val = 0
+        events.sort(key=lambda x:(x[1],-x[2]))
+        #print(events)
+
+        max_value = []
+        cur = 0
+        for _, end, value in events:
+            cur = max(cur,value)
+            max_value.append((end,cur))
+        #print(max_value)
         ends = [end for _, end, _ in events]
-        for i in range(n):
-            cur = events[i][2]
-            start = events[i][0]
-            idx = bisect.bisect_right(ends, start - 1) - 1 
-            if idx == -1:
-                max_val = max(max_val, cur)
-                continue
-            
-            cur += max_value_up_to[idx][1]
-            max_val = max(max_val, cur)
-        return max_val
+        res = 0
+        for start, end, value in events:
+            idx = bisect.bisect_right(ends, start - 1) - 1
+            if idx >= 0:
+                res = max(res, max_value[idx][1] + value)
+            else:
+                res = max(res, value)
+
+        return res
