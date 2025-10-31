9889bd7866c028fa0a5f873d1f1f7976c42bec6a
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-10-31 17:08:48 -0400

    Time: 39 ms (5.88%), Space: 19.6 MB (0%) - LeetHub

--- 1324-where-will-the-ball-fall/1324-where-will-the-ball-fall.py
+++ 1324-where-will-the-ball-fall/1324-where-will-the-ball-fall.py
@@ -6,18 +6,36 @@ class Solution(object):
         """
         rows = len(grid)
         cols = len(grid[0])
+        dp = {}
         def dfs(i, j):
+            if (i,j) in dp:
+                return dp[(i, j)]
+
             if i == rows:
                 return j
 
             if grid[i][j] == 1:
                 if j + 1 >= cols or grid[i][j + 1] == -1:
                     return -1
-                return dfs(i + 1, j + 1)
+                ret = dfs(i + 1, j + 1)
+                if ret == -1:
+                    dp[(i, j)] = -1
+                else:
+                    dp[(i, j)] = ret
+                return dp[(i, j)]
+                
             if grid[i][j] == -1:
                 if j -1 < 0 or grid[i][j - 1] == 1:
                     return -1
-                return dfs(i + 1, j - 1)
+                ret = dfs(i + 1, j - 1)
+                if ret == -1:
+                    dp[(i, j)] = -1
+                else:
+                    dp[(i, j)] = ret
+                
+                return dp[(i, j)]
+
+
         
         ans = []
         for i in range(cols):
