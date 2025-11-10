c17b20dbbb4930fcfabcdea96b813e41502cfe11
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-11-10 11:44:37 -0500

    Time: 260 ms (9.09%), Space: 13.2 MB (54.55%) - LeetHub

---
+++ 1261-swap-for-longest-repeated-character-substring/1261-swap-for-longest-repeated-character-substring.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def maxRepOpt1(self, text):
+        """
+        :type text: str
+        :rtype: int
+        """
+        # Outer loop, iterate from a - z
+        # Inner loop, linear scan of sliding window ->  tolerant one difference
+
+        n = len(text)
+        count = Counter(text)
+        max_len = 0
+
+        for i in range(26):
+            ch = chr(ord('a') + i)
+            left = 0
+            tolerant = 0
+            max_count = count[ch]
+            for right in range(n):
+                if text[right] != ch:
+                    tolerant += 1
+                
+                if tolerant <= 1:
+                    max_len = max(max_len, right - left + 1)
+                    if  right - left + 1 == max_count:
+                        break
+                while tolerant > 1:
+                    # abbaa
+                    if text[left] != ch:
+                        tolerant -= 1
+                    left += 1
+        return max_len
+
+
+
+
