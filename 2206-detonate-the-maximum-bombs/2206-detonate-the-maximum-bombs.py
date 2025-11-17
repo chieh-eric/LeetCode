0ecdcf50aeb49b43b0086019b79b47bf734980cf
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-11-17 14:13:36 -0500

    Time: 4103 ms (5.21%), Space: 12.6 MB (91.08%) - LeetHub

--- 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.py
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.py
@@ -5,46 +5,35 @@ class Solution(object):
         :type bombs: List[List[int]]
         :rtype: int
         """
-        graph = defaultdict(list)
+        # Build graph -> x, y, r
+        # dfs -> return number of bombs will exploded (Memorization)
+
+        # graph = defaultdict(list)
+        # for x, y, r in bombs:
+        #     graph
+    
+        #memo = {}
         n = len(bombs)
-        distance = {}
+        def dfs(index, path):
+            #if index in memo:
+             #   return memo[index]
 
-        def calculate(s1,s2):
-            way = 0
-            x1, y1, r1 = s1[0], s1[1], s1[2]
-            x2, y2, r2 = s2[0], s2[1], s2[2]
-            if (x1,y1,x2,y2) in distance:
-                way= distance[(x1,y1,x2,y2)]
-            else:
-                way = ((x1-x2)**2 + (y1-y2)**2)**0.5
-                distance[(x1,y1,x2,y2)] = way
-                distance[(x2,y2,x1,y1)] = way
-            if way <= r1:
-                return True
-            return False
-
-            
-        for i in range(n):
-            for j in range(n):
-                if i != j:
-                    if calculate(bombs[i],bombs[j]):
-                        graph[i].append(j)
+            path.add(index)
+            x, y, r = bombs[index]
+            count = 1
+            for i in range(n):
+                if i == index:
+                    continue
+                nei_x, nei_y, _ = bombs[i]
+                if (nei_x - x)**2 + (nei_y - y)**2 <= r**2 and  i not in path:
+                    count = count + dfs(i, path)
+            #memo[index] = count
+            return count
         
-        def dfs(node):
-            if node in visited:
-                return 0
-            visited.add(node)
-            found = 1
-            if graph[node]:
-                for nei in graph[node]:
-                    if nei not in visited:
-                        ans = dfs(nei)
-                        found += ans
-            return found
-
-        max_length = 0
+        
+        max_count = 0
         for i in range(n):
-            visited = set()
-            res = dfs(i)
-            max_length = max(max_length,res)
-        return max_length
+            #visited = set()
+            max_count = max(max_count, dfs(i, set()))
+        #print(memo)
+        return max_count
