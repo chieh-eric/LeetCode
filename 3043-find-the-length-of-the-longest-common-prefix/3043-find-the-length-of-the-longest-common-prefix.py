3133d0786aa8f7170940051aeb36f1e7d6cec35f
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-11-28 14:33:00 -0500

    Time: 508 ms (51.16%), Space: 24.1 MB (43.6%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
@@ -1,7 +1,3 @@
-class Node():
-    def __init__(self):
-        self.children = {}
-
 class Solution(object):
     def longestCommonPrefix(self, arr1, arr2):
         """
@@ -9,43 +5,19 @@ class Solution(object):
         :type arr2: List[int]
         :rtype: int
         """
-        # 1
-        # 0
-        # 0
-        tree1 = Node()
+        prefix_arr1 = set()
+
         for val in arr1:
-            word = str(val)
-            cur = tree1
-            for ch in word:
-                if ch not in cur.children:
-                    cur.children[ch] = Node()
-                cur = cur.children[ch]
-
-        tree2 = Node()
-        for val in arr2:
-            word = str(val)
-            cur = tree2
-            for ch in word:
-                if ch not in cur.children:
-                    cur.children[ch] = Node()
-                cur = cur.children[ch]
+            s = str(val)
+            for i in range(1,len(s)+1):
+                prefix_arr1.add(s[:i])
         
-        self.max_depth = 0
-        def dfs(pos1, pos2, d):
-            self.max_depth = max(self.max_depth, d)
-            for child1 in pos1.children:
-                for child2 in pos2.children:
-                    if child1 == child2:
-                        dfs(pos1.children[child1], pos2.children[child2],d+1)
-        dfs(tree1, tree2, 0)
-        return self.max_depth
+        ans = 0
 
-
-
-
-
-        # 1
-        # 0
-        # 0
-        # 0
+        for val in arr2:
+            s = str(val)
+            for i in range(1, len(s)+1):
+                if s[:i] in prefix_arr1:
+                    ans = max(ans, i)
+        return ans
 
