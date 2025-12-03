55b22f0f1f5fec95b5fa2900e0cd056f8e2fc367
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-12-03 15:30:39 -0500

    Time: 590 ms (81.36%), Space: 22.3 MB (74.58%) - LeetHub

--- 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.py
+++ 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.py
@@ -6,23 +6,18 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        # [4] , min_val = 2
-        # [3,4], min_val = 0
-        sort_list = []
-        min_val = float('inf')
-        for i, val in enumerate(nums):
-            if i < x:
-                continue
-            new_element = nums[i-x]
-            insort(sort_list, new_element)
-            idx = bisect.bisect_left(sort_list,val)
-            #print(sort_list, val)
+        arr = []
+        n = len(nums)
+        min_diff = float('inf')
 
-            if idx < len(sort_list):
-                min_val = min(min_val, abs(val - sort_list[idx]))
+        for i in range(x, n):
+            bisect.insort(arr,nums[i-x])
+            idx = bisect.bisect_left(arr, nums[i])
+
+            if idx < len(arr):
+                min_diff = min(min_diff, abs(nums[i] - arr[idx]))
             
             if idx > 0:
-                min_val = min(min_val, abs(val-sort_list[idx-1]))
-        return min_val
-
-        
+                min_diff = min(min_diff, abs(nums[i] - arr[idx-1]))
+                
+        return min_diff
