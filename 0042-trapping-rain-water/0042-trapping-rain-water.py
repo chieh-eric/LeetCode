3e7ad106a5003ee5b7ba3fdbf6f884b5ba73e307
Strokeric <82665820+chieh-eric@users.noreply.github.com>
2025-12-09 22:37:44 -0500

    Time: 24 ms (32.82%), Space: 13.5 MB (61.9%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -5,16 +5,11 @@ class Solution(object):
         :rtype: int
         """
         stack = []
-        # [(1,0),(0,1),]
-        # trap = (prevHeight - popOutHeight)*(index - prevIndex - 1)
-        trap = 0
-        for i, val in enumerate(height):
-            while stack and stack[-1][0] < val:
-                popOutHeight = stack[-1][0]
-                stack.pop()
+        res = 0
+        for i, h in enumerate(height):
+            while stack and height[stack[-1]] < h:
+                popIndex = stack.pop()
                 if stack:
-                    trap += (min(stack[-1][0],val) - popOutHeight)*(i - stack[-1][1] - 1)
-                #print(trap)
-            #print(trap)
-            stack.append((val, i))
-        return trap
+                    res += ((min(height[stack[-1]], h) - height[popIndex])*(i-stack[-1]-1))
+            stack.append(i)
+        return res
